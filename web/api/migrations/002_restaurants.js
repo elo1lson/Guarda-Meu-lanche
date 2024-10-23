e2d103f113452678deffde514c14d622ae002a00
Pedro Henrique Vieira Barreto <112330570+Masterbarreto@users.noreply.github.com>
2024-10-23 10:02:07 -0300

    fet: coreção de emergencia

--- web/api/migrations/002_restaurants.js
+++ web/api/migrations/002_restaurants.js
@@ -1,21 +1,15 @@
 import { tableNames } from "../knex/tableNames.js";
 
 export const up = function (knex) {
-    return knex.schema.createTable(tableNames.restaurants, (t) => {
-        t.bigIncrements("id").primary().notNullable();
-        t.string("name", 250).notNullable();
-        t.bigInteger("area_id");
-        t.string("logo", 2048).notNullable();
-        t.bigInteger("owner_id");
-        t.foreign("owner_id").references("id").inTable(tableNames.users);
-        t.foreign("area_id")
-            .references("id")
-            .inTable(tableNames.food_area)
-            .onDelete("CASCADE");
-        t.timestamp("created_at").defaultTo(knex.fn.now());
-    });
+  return knex.schema.createTable(tableNames.restaurants, (t) => {
+    t.bigIncrements("id").primary().notNullable();
+    t.string("name", 250).notNullable();
+    t.bigInteger("area_id");
+    t.foreign("area_id").references("id").inTable(tableNames.food_area).onDelete("CASCADE");
+    t.timestamp("created_at").defaultTo(knex.fn.now());
+  });
 };
 
 export const down = function (knex) {
-    return knex.schema.dropTable(tableNames.restaurants);
+  return knex.schema.dropTable(tableNames.restaurants);
 };
